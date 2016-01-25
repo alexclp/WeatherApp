3cf5e7e740a721b705241c152246d7338c4b3527
alexclp <alexandru.clapa@gmail.com>
2016-01-25 15:28:26 +0000

    Files for custom table view cell

---
+++ Weather/WeatherForecastCustomCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherForecastCustomCell.swift
+//  Weather
+//
+//  Created by Alexandru Clapa on 25/01/2016.
+//  Copyright © 2016 Alexandru Clapa. All rights reserved.
+//
+
+import UIKit
+
+class WeatherForecastCustomCell: UITableViewCell {
+
+}
