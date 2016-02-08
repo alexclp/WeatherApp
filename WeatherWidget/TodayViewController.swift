b99167f6c6f22476149a1cf3accb8d87c76d5d31
alexclp <alexandru.clapa@gmail.com>
2016-02-08 17:46:50 +0000

    UI Elements to the Today extension

--- WeatherWidget/TodayViewController.swift
+++ WeatherWidget/TodayViewController.swift
@@ -10,15 +10,15 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+	
+	@IBOutlet weak var weatherImage: UIImageView?
+	@IBOutlet weak var currentTempLabel: UILabel?
+	@IBOutlet weak var minTempLabel: UILabel?
+	@IBOutlet weak var maxTempLabel: UILabel?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)) {
